c227fc3956ff695c59ae313f4924d4ec97a2ed59
Carolyn Zhang <czhang92@gmail.com>
2013-02-24 15:51:44 -0500

    Created new RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarolynzProj1::Application.config.secret_token = '6ad62a1c5b4b6b7bb654ab70b9bd09d8ace8bbd125a614bf835459fc7dc5e30fc4267ea0640b13f168791774f883715803aa2df79fc0bb9f278a793ae7f9b30e'
